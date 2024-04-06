57ee68e9e4ed4e5018617340e03cc7e127d5db58
Prekshagupta99 <prekshamanglagupta@gmail.com>
2024-04-06 23:15:06 +0530

    frontend setup and final commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
+import { ThemeProvider } from "@mui/material/styles";
+import MDBox from "components/MDBox";
+import Sidenav from "examples/Sidenav";
+import theme from "assets/theme";
+import themeDark from "assets/theme-dark";
+import routes from "routes";
+import {
+  setMiniSidenav,
+  setOpenConfigurator,
+  useMaterialUIController,
+} from "context";
+import brandDark from "assets/images/logo-ct-dark.png";
+import brandWhite from "assets/images/logo-ct.png";
+
+export default function App() {
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    direction,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const { pathname } = useLocation();
+
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  const handleConfiguratorOpen = () =>
+    setOpenConfigurator(dispatch, !openConfigurator);
+
+  useEffect(() => {
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
+
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return (
+          <Route
+            exact
+            path={route.route}
+            element={route.component}
+            key={route.key}
+          />
+        );
+      }
+
+      return null;
+    });
+
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {layout === "dashboard" && (
+        <Sidenav
+          color={sidenavColor}
+          brand={
+            (transparentSidenav && !darkMode) || whiteSidenav
+              ? brandDark
+              : brandWhite
+          }
+          brandName="Project Name"
+          routes={routes}
+          onMouseEnter={handleOnMouseEnter}
+          onMouseLeave={handleOnMouseLeave}
+        />
+      )}
+
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
---
+++ src/layouts/dashboard/components/Projects/data/index.js
@@ -0,0 +1,147 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+
+
+// @mui material components
+import Tooltip from "@mui/material/Tooltip";
+import MDAvatar from "components/MDAvatar";
+import MDBox from "components/MDBox";
+import MDProgress from "components/MDProgress";
+import MDTypography from "components/MDTypography";
+
+// Images
+import logoAtlassian from "assets/images/small-logos/logo-atlassian.svg";
+import logoInvesion from "assets/images/small-logos/logo-invision.svg";
+import logoJira from "assets/images/small-logos/logo-jira.svg";
+import logoSlack from "assets/images/small-logos/logo-slack.svg";
+import logoSpotify from "assets/images/small-logos/logo-spotify.svg";
+import logoXD from "assets/images/small-logos/logo-xd.svg";
+import team1 from "assets/images/team-1.jpg";
+import team2 from "assets/images/team-2.jpg";
+import team3 from "assets/images/team-3.jpg";
+import team4 from "assets/images/team-4.jpg";
+
+export default function data() {
+  const avatars = (members) =>
+    members.map(([image, name]) => (
+      <Tooltip key={name} title={name} placeholder="bottom">
+        <MDAvatar
+          src={image}
+          alt="name"
+          size="xs"
+          sx={{
+            border: ({ borders: { borderWidth }, palette: { white } }) =>
+              `${borderWidth[2]} solid ${white.main}`,
+            cursor: "pointer",
+            position: "relative",
+
+            "&:not(:first-of-type)": {
+              ml: -1.25,
+            },
+
+            "&:hover, &:focus": {
+              zIndex: "10",
+            },
+          }}
+        />
+      </Tooltip>
+    ));
+
+  const Company = ({ image, name }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar src={image} name={name} size="sm" />
+      <MDTypography variant="button" fontWeight="medium" ml={1} lineHeight={1}>
+        {name}
+      </MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "companies", accessor: "companies", width: "45%", align: "left" },
+      { Header: "budget", accessor: "budget", align: "center" },
+      { Header: "completion", accessor: "completion", align: "center" },
+    ],
+
+    rows: [
+      {
+        companies: <Company image={logoXD} name="Material UI XD Version" />,
+        budget: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            $14,000
+          </MDTypography>
+        ),
+        completion: (
+          <MDBox width="8rem" textAlign="left">
+            <MDProgress value={60} color="info" variant="gradient" label={false} />
+          </MDBox>
+        ),
+      },
+      {
+        companies: <Company image={logoAtlassian} name="Add Progress Track" />,
+        budget: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            $3,000
+          </MDTypography>
+        ),
+        completion: (
+          <MDBox width="8rem" textAlign="left">
+            <MDProgress value={10} color="info" variant="gradient" label={false} />
+          </MDBox>
+        ),
+      },
+      {
+        companies: <Company image={logoSlack} name="Fix Platform Errors" />,
+        budget: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            Not set
+          </MDTypography>
+        ),
+        completion: (
+          <MDBox width="8rem" textAlign="left">
+            <MDProgress value={100} color="success" variant="gradient" label={false} />
+          </MDBox>
+        ),
+      },
+      {
+        companies: <Company image={logoSpotify} name="Launch our Mobile App" />,
+        budget: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            $20,500
+          </MDTypography>
+        ),
+        completion: (
+          <MDBox width="8rem" textAlign="left">
+            <MDProgress value={100} color="success" variant="gradient" label={false} />
+          </MDBox>
+        ),
+      },
+      {
+        companies: <Company image={logoJira} name="Add the New Pricing Page" />,
+        budget: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            $500
+          </MDTypography>
+        ),
+        completion: (
+          <MDBox width="8rem" textAlign="left">
+            <MDProgress value={25} color="info" variant="gradient" label={false} />
+          </MDBox>
+        ),
+      },
+      {
+        companies: <Company image={logoInvesion} name="Redesign New Online Shop" />,
+        budget: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            $2,000
+          </MDTypography>
+        ),
+        completion: (
+          <MDBox width="8rem" textAlign="left">
+            <MDProgress value={40} color="info" variant="gradient" label={false} />
+          </MDBox>
+        ),
+      },
+    ],
+  };
+}
